f398d99592b20bb8376cc0ab011a5c98e0e5e845
Gabriel Afini <gabrielsads@gmail.com>
2025-07-12 11:47:55 -0300

    Feat: criado laço de repetição, para só sair do programa quando o usuario desejar

--- Program.cs
+++ Program.cs
@@ -6,48 +6,51 @@ Calculadora calc = new Calculadora();
 
 opc = calc.Menu();
 
-switch (opc)
+do
 {
-    case 1:
-        calc.Somar();
-        break;
-    case 2:
-        calc.Subtrair();
-        break;
-    case 3:
-        calc.Multiplicar();
-        break;
-    case 4:
-        calc.Dividir();
-        break;
-    case 5:
-        calc.Potencia();
-        break;
-    case 6:
-        calc.Raiz();
-        break;
-    case 7:
-        calc.X = 0;
-        calc.Seno();
-        break;
-    case 8:
-        calc.X = 0;
-        calc.Cosseno();
-        break;
-    case 9:
-        calc.X = 0;
-        calc.Tangente();
-        break;
-    case 10:
-        calc.X = 0;
-        calc.Seno();
-        calc.Cosseno();
-        calc.Tangente();
-        break;
-    default:
-        if (opc != 0)
-        {
-            Console.WriteLine("Valor invalido.");
-        }
-        break;
-}
+    switch (opc)
+    {
+        case 1:
+            calc.Somar();
+            break;
+        case 2:
+            calc.Subtrair();
+            break;
+        case 3:
+            calc.Multiplicar();
+            break;
+        case 4:
+            calc.Dividir();
+            break;
+        case 5:
+            calc.Potencia();
+            break;
+        case 6:
+            calc.Raiz();
+            break;
+        case 7:
+            calc.X = 0;
+            calc.Seno();
+            break;
+        case 8:
+            calc.X = 0;
+            calc.Cosseno();
+            break;
+        case 9:
+            calc.X = 0;
+            calc.Tangente();
+            break;
+        case 10:
+            calc.X = 0;
+            calc.Seno();
+            calc.Cosseno();
+            calc.Tangente();
+            break;
+        default:
+            if (opc != 0)
+            {
+                Console.WriteLine("Valor invalido.");
+            }
+            break;
+    }
+} while (opc != 0);
